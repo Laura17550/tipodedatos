0e634e57642608f24287ce5dd399e2c177f7ca75
Laura17550 <laulow17@gmail.com>
2024-09-21 14:42:13 -0600

    practica clase de datos
    tamaño de datos

--- tiposdedatos/src/tiposdedatos/Tiposdedatos.java
+++ tiposdedatos/src/tiposdedatos/Tiposdedatos.java
@@ -16,8 +16,21 @@ public class Tiposdedatos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("holamundo");
-System.out.println("holamundo");
-System.out.println("holamundo");
+        byte ejemplobyte = -128;
+       //el valor de byte es de -128 y +127 
+       
+        System.out.println("valor minimo " + Byte.MIN_VALUE + " valor maximo " + Byte.MAX_VALUE);
+        
+        short ejemploshort = 32767;
+        System.out.println("valor minimo " + Short.MIN_VALUE + " valor maximo " + Short.MAX_VALUE);
+        
+        int ejemploint = 2147483647;
+        System.out.println("valor minimo " + Integer.MIN_VALUE + " valor maximo " + Integer.MAX_VALUE);
+        
+        
     }
     
+    
+    
+   
 }
